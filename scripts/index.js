595221dc9b28c6720ae4e7397caecce3710a838d
redassser <redryan890@gmail.com>
2023-10-24 18:37:47 -0400

    cleaner

--- scripts/index.js
+++ scripts/index.js
@@ -4,52 +4,67 @@ const statLib = {
     "rr:g17": {
         semi: true,
         magSize: 17,
-        maxDistance: 40, //blocks
+        maxDistance: 90, //blocks
         damage: 8, //half hearts
         intensity: 2
     },
-    "rr:m1a1": {
+    "rr:m4a1": {
         semi: false,
-        magSize: 17,
-        maxDistance: 4000, //blocks
-        damage: 8, //half hearts
+        magSize: 30,
+        maxDistance: 300, //blocks
+        damage: 6, //half hearts
         intensity: 4
     }
 }
-const list = (function () {return Object.keys(statLib)})();
 
 //world.afterEvents.itemStartUse()
 world.afterEvents.itemUse.subscribe((e) => {
-    if (!list.includes(e.itemStack.typeId)) return;
-    const player = e.source;
+    trigger(e);
+})
+function trigger(e) {
+    if (!e.itemStack || !e.itemStack.hasTag("rr:gun")) return;
+    const ammo = e.itemStack.getComponent("minecraft:durability");
     const opts = statLib[e.itemStack.typeId];
-    const playerhead = player.getHeadLocation();
+    if (ammo.maxDurability - ammo.damage > 0) { // Ammo
+        //e.itemStack.triggerEvent("rr:takedamage")
+        fire(e, opts);
+    } else { //No ammo
+        misfire(e)
+    }
+}
+function fire(e, opts) {
+    //Calculate Hits
+    const playerhead = e.source.getHeadLocation(), playerview = e.source.getViewDirection();
     playerhead.y += 0.1;
-    const blk = player.dimension.getBlockFromRay(playerhead, player.getViewDirection(), { maxDistance: opts.maxDistance });
-    const ent = player.getEntitiesFromViewDirection({ maxDistance: opts.maxDistance })[0];
-    
-    if (!blk && !ent) { player.sendMessage("mis"); } // TOTAL MISS!!
+    const blk = e.source.dimension.getBlockFromRay(playerhead, playerview, { maxDistance: opts.maxDistance });
+    const ent = e.source.dimension.getEntitiesFromRay(playerhead, playerview, { maxDistance: opts.maxDistance })[1];
+
+    if (!blk && !ent) return; // Miss
     else if (ent && (!blk || !blk.distance || ent.distance <= blk.distance) && !ent.entity.hasComponent("minecraft:item")) {
-        ent.entity.applyDamage(opts.damage, { cause: "entityAttack", damagingEntity: player });
+        ent.entity.applyDamage(opts.damage, { cause: "entityAttack", damagingEntity: e.source });
         //player.dimension.spawnParticle("rr:blood", ent.entity.getHeadLocation());
     } else if (blk && (!ent || ent.distance > blk.distance)) {
         let newLoc = blk.block.location;
         let face = { x: 0, y: 0, z: 0 };
+        let dir;
         newLoc.x += blk.faceLocation.x; newLoc.y += blk.faceLocation.y; newLoc.z += blk.faceLocation.z;
         const vars = new MolangVariableMap();
         switch (blk.face) {
-            case "East": face.x = 1; newLoc.x += .01; break;
-            case "West": face.x = -1; newLoc.x -= .01; break;
-            case "Up": face.y = 1; newLoc.y += .01; break;
-            case "Down": face.y = -1; newLoc.y -= .01; break;
-            case "South": face.z = 1; newLoc.z += .01; break;
-            case "North": face.z = -1; newLoc.z -= .01; break;
+            case "East": face.x = 1; dir = "x"; break;
+            case "West": face.x = -1; dir = "x"; break;
+            case "Up": face.y = 1; dir = "y"; break;
+            case "Down": face.y = -1; dir = "y"; break;
+            case "South": face.z = 1; dir = "z"; break;
+            case "North": face.z = -1; dir = "z"; break;
         }
         vars.setVector3("variable.direction", face);
         vars.setFloat("variable.intensity", opts.intensity)
 
-        player.dimension.spawnParticle("rr:bulletexp", newLoc, vars);
-        player.dimension.spawnParticle("rr:bullethole", newLoc);
-        player.dimension.spawnParticle("rr:bulletspray", newLoc, vars);
+        e.source.dimension.spawnParticle("rr:bulletexp", newLoc, vars);
+        e.source.dimension.spawnParticle("rr:bullethole"+dir, newLoc, vars);
+        e.source.dimension.spawnParticle("rr:bulletspray", newLoc, vars);
     }
-})
+}
+function misfire(e) {
+    e.source.sendMessage("misf")
+}
